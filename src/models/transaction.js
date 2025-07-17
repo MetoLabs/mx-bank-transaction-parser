c68a688a3cf92a78219adb45d0911038d106532d
Gerardo Ramirez <gera.96@icloud.com>
2025-07-17 11:26:36 -0600

    first commit

---
+++ src/models/transaction.js
@@ -0,0 +1,43 @@
+/**
+ * Represents a bank transaction.
+ */
+export class Transaction {
+    /**
+     * Creates a new Transaction instance.
+     *
+     * @param {Object} params
+     * @param {string} params.date - Transaction date string, format depends on bank
+     * @param {string} params.type - Transaction type (e.g. 'Cargo', 'Abono')
+     * @param {number} params.amount - Transaction amount
+     * @param {number} params.balance - Account balance after transaction
+     * @param {string} params.description - Description or concept of transaction
+     * @param {string} params.reference - Reference or tracking number
+     * @param {string} params.bank - Bank involved in transaction
+     * @param {string|null} [params.beneficiary] - Beneficiary of the transaction, if any
+     * @param {string|null} [params.trackingKey] - Internal tracking key, if any
+     * @param {string} [params.extra] - Raw original line or extra info
+     */
+    constructor({
+                    date,
+                    type,
+                    amount,
+                    balance,
+                    description,
+                    reference,
+                    bank,
+                    beneficiary = null,
+                    trackingKey = null,
+                    extra = '',
+                }) {
+        this.date = date;
+        this.type = type;
+        this.amount = amount;
+        this.balance = balance;
+        this.description = description;
+        this.reference = reference;
+        this.bank = bank;
+        this.beneficiary = beneficiary;
+        this.trackingKey = trackingKey;
+        this.extra = extra;
+    }
+}
